ab3ee075a229b89f2fb17a653366529e5e6bcbd5
tcely <tcely@users.noreply.github.com>
2018-05-18 09:46:45 -0400

    Add restart-bluetooth
    Usage:
        restart-bluetooth.sh [wifi]
    
    With the "wifi" argument, the Wi-Fi device is also turned off 
    then on again after a short delay.
    
    With MacOS X 10.13, a lot of the time my headphones won't be seen 
    / connected.  This script solves that problem for me much more
    reliably than anything else I've found.
    
    Having both Wi-Fi and Bluetooth off for a short time seems to 
    clear up most of the issues I'm seeing. But that's very
    disruptive so I tend to try just resetting the Bluetooth first.
    Sometimes, I need to do both to get it working again.
    
    The fact that my MacBook Pro doesn't initiate a connection to my 
    headphones after enabling Bluetooth is extremely annoying!!
    
    Without this script I'm forced to hit the buttons on the 
    headphones myself to try to connect multiple times since the 
    connection often fails in this direction and if I don't the
    headphones power off without any sounds leaving me to guess if
    they are on or not while I'm trying to reconnect.

---
+++ restart-bluetooth.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Restart Bluetooth (and optionally wifi too) then connect to paired devices.
+#
+# Usage:
+#     restart-bluetooth.sh [wifi]
+#
+# Requires:
+#     awk
+#     networksetup
+#     sleep
+#     xargs
+#     https://github.com/breiter/blueutil
+#     https://github.com/lapfelix/BluetoothConnector
+
+get_bt_state() {
+    local _awk_program='{print $NF; exit;}'
+
+    blueutil status | awk "$_awk_program"
+}
+
+[ "on" = "$(get_bt_state)" ] || exit 0
+
+get_wifi_dev() {
+    local _awk_program='$NF ~ /^Wi-Fi$/ {getline; print $2;}'
+
+    networksetup -listallhardwareports | awk "$_awk_program"
+}
+
+if [ "wifi" = "$1" ]; then
+    blueutil off
+
+    _wifi_dev="$(get_wifi_dev)"
+    if [ -n "$_wifi_dev" ]; then
+	networksetup -setairportpower "$_wifi_dev" off
+	sleep 2
+	networksetup -setairportpower "$_wifi_dev" on
+    fi
+
+    blueutil on
+else
+    blueutil restart
+fi
+
+BluetoothConnector | awk 'NR>=5 {print $1}' | xargs -P 2 -n 1 BluetoothConnector
